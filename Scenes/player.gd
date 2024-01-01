e7914984e4d81dd744ba913d77fa2dba80ab2d52
Hassan-A <hassan-all@hotmail.com>
2024-01-01 10:46:12 -0500

    Add speed const

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+const SPEED = 20
+
 func _physics_process(delta):
 	var direction = Vector2(0,0)
 	
 	if Input.is_action_pressed("ui_up"):
 		direction = Vector2(0,-1)
-	velocity = direction * 10
+	velocity = direction * SPEED
 	move_and_slide()
