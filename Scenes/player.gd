2b1c3154c70fc3e5fab76ec7716af789dcd565de
Hassan-A <hassan-all@hotmail.com>
2024-01-01 10:45:42 -0500

    Fix movement backwards

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,6 +4,6 @@ func _physics_process(delta):
 	var direction = Vector2(0,0)
 	
 	if Input.is_action_pressed("ui_up"):
-		direction = Vector2(0,1)
-	velocity = direction * 5
+		direction = Vector2(0,-1)
+	velocity = direction * 10
 	move_and_slide()
