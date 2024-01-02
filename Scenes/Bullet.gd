0f9476603411edf6498c25684b020dbb1e974a42
Hassan-A <hassan-all@hotmail.com>
2024-01-02 09:38:30 -0500

    Add bullet script movement

--- Scenes/Bullet.gd
+++ Scenes/Bullet.gd
@@ -1,1 +1,10 @@
 extends CharacterBody2D
+const SPEED = 500.0
+var direction = Vector2(0,0)
+func _physics_process(delta):
+	velocity = direction * SPEED
+	
+	move_and_slide()
+
+func create(dir):
+	direction = dir
