50a5e3e830841df3fded4ae03ee3adba6384fb63
Hassan-A <hassan-all@hotmail.com>
2024-01-04 14:41:51 -0500

    Spawn asteroids raandomly

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,9 +1,14 @@
 extends Node2D
-
+var random = RandomNumberGenerator.new()
 @export var playerScene : Resource
+var astroidCount = 0
+var astroidDict : Dictionary
+@export var asteroidScene : Resource
 
 func _ready():
+	random.randomize()
 	playerScene = preload("res://Prefabs/player.tscn")
+	asteroidScene = preload("res://Prefabs/asteroid.tscn")
 	spawn_player()
 
 func spawn_player():
@@ -14,3 +19,19 @@ func spawn_player():
 
 func bullets(b):
 	add_child(b)
+
+func create_asteroids():
+	if(astroidCount < 10):
+		var ast = asteroidScene.instantiate()
+		astroidDict[ast] = 1
+		astroidCount += 1
+		add_child(ast)
+		var startingPosition := Vector2(0,0)
+		if(random.randf() > 0.5):
+			startingPosition.x = random.randf_range(0,1152)
+			startingPosition.y = 0
+		elif(random.randf() > 0.5):
+			startingPosition.x = 0
+			startingPosition.y = random.randf_range(0,648)
+		ast.position = startingPosition
+		ast.set_direction(Vector2(random.randf_range(-1,1),random.randf_range(-1,1)))
