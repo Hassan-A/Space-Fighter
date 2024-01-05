4e58c8db116c923cf7f302152a1325097c9b1820
Hassan-A <hassan-all@hotmail.com>
2024-01-05 12:35:31 -0500

    Add asteroid collision

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -1,10 +1,15 @@
 extends StaticBody2D
 
+
 # Called when the node enters the scene tree for the first time.
 var direction = Vector2(0,0)
 const SPEED = 150.0
 @export var sprite : AnimatedSprite2D
+@export var colShape : CollisionShape2D
 signal destroyed
+func _ready():
+	pass # Replace with function body.
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = position + direction * SPEED * delta
@@ -13,10 +18,12 @@ func set_direction(new_direction):
 	direction = new_direction
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	colShape.disabled = true
 	emit_signal("destroyed", self)
 	queue_free()
 
 func explode():
+	colShape.disabled = true
 	sprite.play("Explode")
 	emit_signal("destroyed", self)
 
